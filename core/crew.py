a5dfcd932c0402790b453d911de136217fa2c3c2
inder-dev-pro <inder.31ssb@gmail.com>
2025-02-25 22:46:45 +0530

    Changes in core

--- core/crew.py
+++ core/crew.py
@@ -1,11 +1,16 @@
 from crewai import Crew
 from IPython.display import Markdown
+from agents.idea_viability_agent import IdeaViabilityAgent
+from tasks.idea_viability_task import IdeaViabilityTask
 from agents.market_research_agent import MarketResearchAgent
 from tasks.research_task import research_task
 topic=input("Enter the topic for market research: ")
+idea=input("Enter the idea for viability analysis: ")
+idea_viability=IdeaViabilityAgent(idea=idea)
+idea_viability_task=IdeaViabilityTask(idea=idea)
 market_research=MarketResearchAgent(topic=topic)
 research_task=research_task(topic=topic)
-crew=Crew(agents=[market_research], tasks=[research_task], verbose=True)
+crew=Crew(agents=[idea_viability], tasks=[idea_viability_task], verbose=True)
 
 result=crew.kickoff()
 
