cfdaaba7c8cc009fa0974277a05917d00474ee0c
inder-dev-pro <inder.31ssb@gmail.com>
2025-02-25 22:56:17 +0530

    Initial Update for the data

--- data/crunchbase_data.py
+++ data/crunchbase_data.py
@@ -1,6 +1,13 @@
 import kagglehub
-
+import pandas as pd
 # Download latest version
 path = kagglehub.dataset_download("chhinna/crunchbase-data")
 
 print("Path to dataset files:", path)
+df=pd.read_csv(path+ r"\acq.csv")
+print(df.head(3))
+print(df.info())
+print(df.describe())
+print(df.columns)
+print(df.isnull().sum())
+print(df.duplicated().sum())
